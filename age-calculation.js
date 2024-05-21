5bdfa2d8fe024b55f7faaaf388d4ebaeac52f945
buğra uysal <uysalbugra134@gmail.com>
2024-05-21 23:56:18 +0300

    dosyalar

---
+++ age-calculation.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+/* hesapla */
+
+
+var d=21;
+var m=5;
+var y=2024;
+
+function hsp( ){
+    var gün =document.getElementById("g").value
+var ay =document.getElementById("a").value
+var yıl =document.getElementById("y").value
+    var d=21;
+    var m=5;
+    var y=2024;
+
+var day=g
+var mount=m-a
+var years=years-y
+
+ document.getElementById("guncel").innerHTML=day
+}
+    
